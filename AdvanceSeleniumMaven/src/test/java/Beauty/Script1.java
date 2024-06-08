5532f0f37fb35232a81d790114d1caac5e6c4b2d
HP <HP@DESKTOP-QFPASDK>
2024-06-08 20:17:47 +0530

    I have added a new Scropt inside Beauty Package

---
+++ AdvanceSeleniumMaven/src/test/java/Beauty/Script1.java
@@ -0,0 +1,14 @@
+package Beauty;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Script1 {
+	
+	@Test
+	
+	public void script1() {
+		Reporter.log("Script1 added",true);
+	}
+
+}
